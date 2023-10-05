284afcbca3cacaa3f4adab3ffd9779d2bc3a96a0
brownboynk <78939104+brownboynk@users.noreply.github.com>
2023-10-05 16:32:44 -0400

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,1 +1,27 @@
+import {
+  Container,
+  Header,
+  Content,
+  Nav,
+  Navbar,
+  FlexboxGrid,
+  Panel,
+  Form,
+  ButtonToolbar,
+  Button,
+  Footer,
+} from "rsuite";
+import "rsuite/dist/rsuite.min.css";
 
+export default function App() {
+  return (
+    <div>
+      <Container>
+        <Header>
+          <Navbar appearance="inverse"
+          style={{ backgroundColor: "green" }}
+        </Header>
+      </Container>
+    </div>
+  )
+}
