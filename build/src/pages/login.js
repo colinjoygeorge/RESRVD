c36f19b0938d8d3dd93b4ab54c349c294d47f1c2
brownboynk <78939104+brownboynk@users.noreply.github.com>
2023-10-09 12:06:22 -0400

    updated header

--- build/src/pages/login.js
+++ build/src/pages/login.js
@@ -1,15 +1,11 @@
 import { 
 	Container, 
-	Header, 
 	Content, 
-	Nav, 
-	Navbar, 
 	FlexboxGrid, 
 	Panel, 
 	Form, 
 	ButtonToolbar, 
 	Button, 
-	Footer, 
 } from "rsuite"; 
 import "rsuite/dist/rsuite.min.css"; 
 
@@ -17,19 +13,6 @@ export default function App() {
 	return ( 
 		<div> 
 			<Container> 
-				<Header> 
-					<Navbar appearance="inverse"
-						style={{ backgroundColor: "purple" }}> 
-						<Navbar.Brand>RESRVD</Navbar.Brand> 
-						<Nav> 
-							<Nav.Item>Home</Nav.Item> 
-							<Nav.Item>About</Nav.Item> 
-						</Nav> 
-						<Nav pullRight> 
-							<Nav.Item>Admin Login</Nav.Item> 
-						</Nav> 
-					</Navbar> 
-				</Header> 
 				<div style={{ textAlign: "center" }}> 
 					<h2>RESRVD</h2> 
 					<h4 style={{ color: "purple" }}> 
